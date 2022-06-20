f497948b7f2c78db3c9f2b1d0c37ec4efea9d641
Suleyman Yilmaz <yilmazsgit@gmail.com>
2022-06-20 22:08:49 +0200

    navigation bug is fixed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect} from 'react';
 import Layout from './layout/Layout';
-import {Routes, Route} from 'react-router-dom';
+import {Routes, Route, useNavigate} from 'react-router-dom';
 import CityContext from './store/CityContext';
 import MainPage from './pages/MainPage';
 import DetailPage from './pages/DetailPage';
 
 function App() {
   const {city} = useContext(CityContext);
+  const navigate = useNavigate();
 
+  useEffect(() => {
+    navigate('/');
+  }, []);
   return (
     <Layout>
       <Routes>
